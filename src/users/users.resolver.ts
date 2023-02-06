14f44e8838a4da7fd67b964081eaf3bfada475f6
ghun131 <hungdh131@gmail.com>
2023-02-06 16:46:35 +0700

    feat: update doc, env db id and use headers

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Context, Query, Resolver } from '@nestjs/graphql';
 import { ListUsersResponse } from '@notionhq/client/build/src/api-endpoints';
 import { UserResponse } from './users.model';
 import { UsersService } from './users.service';
@@ -8,7 +8,8 @@ export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => UserResponse)
-  async users(): Promise<ListUsersResponse> {
+  async users(@Context() context): Promise<ListUsersResponse> {
+    console.log('context', context.req.headers);
     const result = await this.usersService.users();
 
     return result;
